259511ef50d67abf97a2e44c29619175a42500d3
Achronferry <805340586@qq.com>
2019-04-23 23:32:44 +0800

    v0.002

--- Client/DAWN/app/src/androidTest/java/com/example/DAWN/ExampleInstrumentedTest.java
+++ Client/DAWN/app/src/androidTest/java/com/example/DAWN/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.myapplication", appContext.getPackageName());
+        assertEquals("com.example.DAWN", appContext.getPackageName());
     }
 }
--- Client/DAWN/app/src/main/java/com/example/DAWN/MainActivity.java
+++ Client/DAWN/app/src/main/java/com/example/DAWN/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void startgame(View view){
+    public void startgame(View view) {
         Intent intent = new Intent(this, ClientGameControl.class);
         startActivity(intent);
     }
