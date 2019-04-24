4baac2df2ab12536cda153348e605410804f675b
Achronferry <805340586@qq.com>
2019-04-24 16:17:03 +0800

    YCY v0.003
    变得线程安全了（大概）

--- Client/DAWN/app/src/main/java/com/example/DAWN/ClientGameControl.java
+++ Client/DAWN/app/src/main/java/com/example/DAWN/ClientGameControl.java
@@ -1,21 +1,23 @@
 package com.example.DAWN;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.*;
 import android.widget.*;
 
+import java.lang.*;
 
 public class ClientGameControl extends AppCompatActivity {
     Intent intent = getIntent();
 
     private Button Lbutton,Rbutton,Ubutton,Dbutton ;
     private ImageView star;
-    //private TextView testtxt ;
+    private TextView testtxt ;
 
-    int vision=20;
-    float[] location={0,0};
+    int vision=20;//视野范围
+    float[] location={0,0}; //当前位置
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +25,14 @@ public class ClientGameControl extends AppCompatActivity {
         setContentView(R.layout.game_process);
 
 
-
-        //testtxt= (TextView) findViewById(R.id.Fortest) ;
-       // testtxt.setText("123");
+        testtxt= (TextView) findViewById(R.id.Fortest) ;
+        testtxt.setText("123");
         star= findViewById(R.id.imageView) ;
 
         location[0]=star.getX();
         location[1]=star.getY();
 
+        //对上下左右进行监听
         Lbutton= (Button) findViewById(R.id.Lbutton);
         Lbutton.setOnTouchListener(new View.OnTouchListener(){
             private Boolean longclicked=false;
@@ -159,12 +161,15 @@ public class ClientGameControl extends AppCompatActivity {
                 return true;
             }
         });
+
+        handler.postDelayed(runnable, 1000 * 5);
+
     }
 
+    //上下左右按键的监听函数
     public void Lmove(){
         location[1]=location[1]-3;
     }
-
     public void Rmove(){
         location[1]=location[1]+3;
     }
@@ -176,5 +181,25 @@ public class ClientGameControl extends AppCompatActivity {
     }
 
 
+    //界面刷新
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable() {
+        public void run() {
+            this.update();
+            handler.postDelayed(this, 20);// 刷新间隔(ms)
+        }
+        void update() {
+            star.setX(location[0]);
+            star.setY(location[1]);
+        }
 
+    };
+
+
+
+    //析构
+    protected void onDestroy() {
+        handler.removeCallbacks(runnable);
+        super.onDestroy();
+    }
 }
