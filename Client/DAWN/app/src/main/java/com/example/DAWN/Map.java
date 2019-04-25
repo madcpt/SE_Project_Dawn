4eb46b270acd6f53861c8c0cd4f3bfd551333c86
Achronferry <805340586@qq.com>
2019-04-25 19:20:29 +0800

    YCY
    Add 了一个map

--- Client/DAWN/app/src/main/java/com/example/DAWN/Map.java
+++ Client/DAWN/app/src/main/java/com/example/DAWN/Map.java
@@ -1,11 +1,16 @@
 package com.example.DAWN;
 
+import java.util.Vector;
 
 public class Map {
     class Role_simple{
         private int id;
+        private  String name;
         private int lifevalue;
         private int[] position;
     }
+    public Vector<Role_simple> livingrole;
+    public Vector<Prop> proplist;
+
 
 }
