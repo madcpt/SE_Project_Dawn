a179b79a5bc3913fa721e8310610f57147513454
madcpt <2436924377@qq.com>
2019-04-30 18:13:03 +0800

    Modify Data.java

--- Client/DAWN/app/src/main/java/com/example/DAWN/Data.java
+++ Client/DAWN/app/src/main/java/com/example/DAWN/Data.java
@@ -4,11 +4,13 @@ public class Data {
     public static Long delay;
     public static String Server;
     public static int port;
+    public static float[] location;
     Data(){
         delay = 0L;
 //        Server = "39.105.27.108";
         Server = "192.168.137.1";
         port = 66;
+        location = new float[]{0, 0};
     }
 
     public static Long getDelay() {
