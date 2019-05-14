b5b614483b66cd78cb7f9f7e9940abf1ce0c8725
Achronferry <805340586@qq.com>
2019-05-14 20:04:00 +0800

    Y_2019.5.14_v2

--- Client/DAWN/app/src/main/java/com/example/DAWN/ClientGameControl.java
+++ Client/DAWN/app/src/main/java/com/example/DAWN/ClientGameControl.java
@@ -16,6 +16,7 @@ import java.util.Arrays;
 
 import android.graphics.*;
 
+
 import com.example.DAWN.Data;
 import com.example.DAWN.DialogManagement.RunnableTCP;
 import com.example.DAWN.DialogManagement.RunnableUDP;
@@ -577,13 +578,13 @@ public class ClientGameControl extends AppCompatActivity {
                             if (Math.abs(r.location[0] - location[0]) > vision * 20 || Math.abs(r.location[1] - location[1]) > vision * 20) {
                                 continue;
                             }
-                            c.drawBitmap(role_pic[r.id%100][r.direction][r.walk_mov],center_location[0] - location[0]+r.location[0],center_location[1] - location[1]+r.location[1],p);
-                            if (r.walk_mov!=0){
-                                r.walk_mov=(r.walk_mov+1)/3;//每个动作循环的帧数
+                            c.drawBitmap(role_pic[r.id%100][r.direction][r.walk_mov/5],center_location[0] - location[0]+r.location[0],center_location[1] - location[1]+r.location[1],p);
+                            if (r.walk_mov!=-1){
+                                r.walk_mov=(r.walk_mov+1)%10;//每个动作循环的帧数(2张动作帧，每帧持续5次刷新）
                             }
                         }
                         //画黑雾
-                        c.saveLayer(0, 0, center_location[0]*2+1, center_location[1]*2+1, p, Canvas.ALL_SAVE_FLAG);//保存上一层
+                        c.saveLayer(0, 0, center_location[0]*2+1, center_location[1]*2+1, p,Canvas.ALL_SAVE_FLAG);//保存上一层
                         p.setColor(Color.BLACK);
                         c.drawRect(0,0,center_location[0]*2+1, center_location[1]*2+1,p);
                         p.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
@@ -592,7 +593,6 @@ public class ClientGameControl extends AppCompatActivity {
                         c.restore();
                     }
                     Thread.sleep(10);
-
                 } catch (Exception e) {
                     e.printStackTrace();
                 }finally{
@@ -600,12 +600,9 @@ public class ClientGameControl extends AppCompatActivity {
                         holder.unlockCanvasAndPost(c);
                     }
                 }
-
-
             }
         }
     }
-
 
 
 
