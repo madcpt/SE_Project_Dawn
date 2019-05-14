7bb17f77aa8fdbef25d70292e82681a85f3d6a87
Achronferry <805340586@qq.com>
2019-05-14 21:10:41 +0800

    Y_2019.5.14_v3

--- Server/TestServer/src/Role.java
+++ Server/TestServer/src/Role.java
@@ -1,16 +1,6 @@
-package com.example.DAWN;
 
 
+import java.lang.*;
 import java.util.Vector;
 
-public class Role {
-    private int roleid;
-    private String roleName;
-    private int attack;
-    private int speed;
-    private int lifevalue;
-    private int maxlife;
-    public Vector<Prop> bag;
-    public Prop weapon;
-    public Prop shoe;
-}
+
