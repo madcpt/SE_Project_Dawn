698214353484edd45b47f87b8ccb3231c4d69faf
YutLan <568485741@qq.com>
2019-05-14 20:36:45 +0800

    2
    2

--- Client/DAWN/app/src/main/java/com/example/DAWN/MainActivity.java
+++ Client/DAWN/app/src/main/java/com/example/DAWN/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import java.util.TimerTask;
 import java.util.Timer;
+import android.os.Handler;
+import android.os.Message;
+
 
 
 
@@ -21,12 +24,25 @@ public class MainActivity extends AppCompatActivity {
     {
 
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
         Timer timer=new Timer();
+        handler.sendEmptyMessageDelayed(0,3000);
 
 
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Jump();
+            super.handleMessage(msg);
+        }
+    };
+
+    public void Jump(){
+        Intent intent = new Intent(this, ClientGameControl.class);
+        startActivity(intent);
+        finish();
     }
 
 
@@ -38,4 +54,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-}
+};
+
+
+
