942cf0d3d2718f5c9c4e3eaaff9c327f0bfa0ce7
YutLan <568485741@qq.com>
2019-05-18 13:19:59 +0800

    5.18.3
    3

--- Client/DAWN/app/src/main/java/com/example/DAWN/CreateRoom.java
+++ Client/DAWN/app/src/main/java/com/example/DAWN/CreateRoom.java
@@ -2,6 +2,7 @@ package com.example.DAWN;
 
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -177,6 +178,8 @@ public class CreateRoom extends AppCompatActivity {
         Intent intent=new Intent(CreateRoom.this,RoomPage.class);
         intent.putExtra("Account",Account);
         startActivity(intent);
+        //onDestroy();
+
     }
 
     //@xzh
@@ -214,6 +217,7 @@ public class CreateRoom extends AppCompatActivity {
             intent.putExtra("Account",Account);
             startActivity(intent);
             new AsyncConTCP ().execute ("new_room," + strtmp2 + "," + strtmp1);
+            onDestroy();
         }
 //        TimeUnit.MILLISECONDS.sleep (500);
 //        new AsyncConTCP ().execute ("chos_r," + strtmp2);
@@ -261,5 +265,9 @@ public class CreateRoom extends AppCompatActivity {
 
 
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
 }
--- Client/DAWN/app/src/main/java/com/example/DAWN/Register.java
+++ Client/DAWN/app/src/main/java/com/example/DAWN/Register.java
@@ -46,11 +46,13 @@ public class Register extends AppCompatActivity {
             switch (v.getId()) {
                 case R.id.register_btn_sure:                       //确认按钮的监听事件
                     register_check();
+                    onDestroy();
                     break;
                 case R.id.register_btn_cancel:                     //取消按钮的监听事件,由注册界面返回登录界面
                     Intent intent_Register_to_Login = new Intent(Register.this,Login.class) ;    //切换User Activity至Login Activity
                     startActivity(intent_Register_to_Login);
                     finish();
+                    onDestroy();
                     break;
             }
         }
