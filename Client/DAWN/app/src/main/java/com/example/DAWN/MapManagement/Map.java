6f6d847cb0757855856504050cac4a21c76b4e5b
madcpt <2436924377@qq.com>
2019-05-19 02:16:24 +0800

    Release v1.0

---
+++ Client/DAWN/app/src/main/java/com/example/DAWN/MapManagement/Map.java
@@ -0,0 +1,34 @@
+package com.example.DAWN.MapManagement;
+
+import com.example.DAWN.RoleManagement.Role_simple;
+
+import java.lang.*;
+
+
+import java.util.Vector;
+
+public class Map {
+    public static int unit =150;
+    public static int size =100;
+
+    public volatile Vector<Role_simple> livingrole;
+    public Vector<Prop> proplist;
+    public int[][] m;
+
+
+
+    public Map() {
+
+        livingrole = new Vector<Role_simple>();
+        proplist = new Vector<Prop>();
+
+    }
+
+    public Boolean update(){
+        //根据传回内容更新各角色数值以及道具情况
+        return true;
+    }
+
+
+
+}
--- Server/TestServer/src/Data.java
+++ Server/TestServer/src/Data.java
@@ -14,7 +14,7 @@ public class Data {
     public Random rand;
     private static Collision Colli = new Collision(120,100);
     public static RoomPool roomList;
-    public static Test database;
+    public static DatabaseAdapter database;
 
     Data() {
         WholeMap = new MapClass();
@@ -114,7 +114,7 @@ public class Data {
         playerLocation = new HashMap<>();
         direction = 0;
         roomList = new RoomPool();
-        database=new Test();
+        database=new DatabaseAdapter();
     }
 
     public void addPlayer(String pureIP, int[] lt) {
