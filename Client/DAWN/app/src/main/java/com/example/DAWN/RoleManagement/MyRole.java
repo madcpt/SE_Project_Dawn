735c4383a15bbed982d93ce1dd9d05a9fcd39c50
Achronferry <805340586@qq.com>
2019-06-18 00:22:08 +0800

    Y_2019.6.17_v3

--- Client/DAWN/app/src/main/java/com/example/DAWN/RoleManagement/MyRole.java
+++ Client/DAWN/app/src/main/java/com/example/DAWN/RoleManagement/MyRole.java
@@ -23,6 +23,14 @@ public class MyRole {
 
             switch (id%100){
                 //不同角色特性
+                case 0:
+                    attack = 45;
+                    speed = 3 ;
+                    break;
+                case 1:
+                    attack = 25 ;
+                    speed = 5 ;
+                    break;
             }
 
         }
